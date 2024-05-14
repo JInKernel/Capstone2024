798df0b753f5555fcccde4aba94c0948cee1d3fb
Joon <tkdwns1088@gmail.com>
2024-05-14 23:26:44 +0900

    장고위에서  만든 front 올라가는 것 확인

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from justfortest import views
+from django.urls import path, include
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', include("justfortest.urls")),
+    path('admin/', admin.site.urls),
 ]
