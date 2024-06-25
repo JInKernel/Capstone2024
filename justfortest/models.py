3e7cdedcc310b6baffa89562b51512eced86bbd4
Joon <117909174+JInKernel@users.noreply.github.com>
2024-06-25 22:22:00 +0900

    Update models.py

--- justfortest/models.py
+++ justfortest/models.py
@@ -10,3 +10,12 @@ class Purchase(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     beverage = models.ForeignKey(Beverage, on_delete=models.CASCADE)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    face_embedding_vector = models.BinaryField()
+    birthdate = models.DateField()
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.user.username
